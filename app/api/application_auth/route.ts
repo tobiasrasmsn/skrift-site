448b5ff480caf5473408be183c1bf7f6d7e95d2a
tobiasrasmsn <tobiasrasmsn@protonmail.com>
2024-08-31 01:13:58 +0200

    update

--- app/api/application_auth/route.ts
+++ app/api/application_auth/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
 // Initialize Supabase client
-const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!);
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!);
 
 export async function POST(request: Request) {
     try {
